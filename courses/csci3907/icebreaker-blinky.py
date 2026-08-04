63ff15e925a279db186015d438abf60eba1878ef
Simon D. Levy <simon.d.levy@gmail.com>
2026-08-04 01:47:32 -0400

    modified:   icebreaker-blinky.py

--- courses/csci3907/icebreaker-blinky.py
+++ courses/csci3907/icebreaker-blinky.py
@@ -1,28 +1,33 @@
-from amaranth import *
-from amaranth.build import Resource, Pins, Attrs
+from amaranth import Elaboratable, Signal, Module
 from amaranth_boards.icebreaker import ICEBreakerPlatform
 
+
 class Blinker(Elaboratable):
+
     def __init__(self):
+
         self.count = Signal(24)
 
     def elaborate(self, platform):
+
         m = Module()
-        
+
         # Get the onboard green LED resource
         led = platform.request("led_g", 0)
-        
+
         # Increment counter on every clock cycle
         m.d.sync += self.count.eq(self.count + 1)
-        
+
         # Drive the LED using the most significant bit of the counter
         m.d.comb += led.o.eq(self.count[-1])
-        
+
         return m
 
+
 if __name__ == "__main__":
+
     # Initialize the iCEBreaker platform definition
     platform = ICEBreakerPlatform()
-    
+
     # Build and flash the design to the board automatically
     platform.build(Blinker(), do_program=True)
